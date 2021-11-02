8f0dcbc6a68edfe445fe8fc611f947c8f2f8190f
atsuki04 <cavu04hmp@gmail.com>
2021-11-02 14:35:55 +0000

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
     root 'homes#top'
     devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end    
   get 'home/about' => 'homes#about'
 end
